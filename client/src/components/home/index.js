b1f39c127bde31ee106d4548a8cd7478018e8b0e
Sudhin Justin <sudhin.justin@advance-auto.com>
2025-04-02 11:46:18 -0400

    chore: updated link

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -54,7 +54,7 @@ export default function Home() {
             </div>
             <div className="box mobile-container">
               <a href="/dev" className="btn btn-outline-dark btn-sm btn-block" style={{ border: '1px solid' }}>Dev Connector Demo</a>
-              <a href="/dev" className="btn btn-outline-dark btn-sm btn-block" style={{ border: '1px solid', marginTop: '20px'}}>View code in Github</a>
+              <a href="https://github.com/justisu/DevConnector/tree/main" className="btn btn-outline-dark btn-sm btn-block" style={{ border: '1px solid', marginTop: '20px'}}>View code in Github</a>
             </div>            
           </div>
         </div>
